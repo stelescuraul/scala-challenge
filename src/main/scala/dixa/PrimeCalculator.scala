cadf584d756cbc89c324a40ff955d9277d8b9ea6
Raul Stelescu <stelescuraul@gmail.com>
2020-08-29 20:43:03 +0200

    feat(prime): add prime calculator

---
+++ src/main/scala/dixa/PrimeCalculator.scala
@@ -0,0 +1,22 @@
+package dixa.test
+
+object PrimeCalculator {
+
+  // Keeping this one public in case anyone would need to use it.
+  // No point in making it private
+  def isPrime(current: Int): Boolean =
+    if (current <= 1)
+      false
+    else if (current == 2)
+      true
+    else
+      !(2 until current).exists(n => current % n == 0)
+
+  // Get the primes up to the upperLimit
+  def getPrimes(upperLimit: Int): Option[LazyList[Int]] = {
+    upperLimit match {
+      case x if x <= 1 => None
+      case _ => Some(2 #:: LazyList.from(3,2).takeWhile(curr =>  curr <= upperLimit ).filter(isPrime))
+    }
+  }
+}

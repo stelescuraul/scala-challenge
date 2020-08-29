5eb139ed02fc20516f9b8950e683ad45def80bcd
Raul Stelescu <stelescuraul@gmail.com>
2020-08-29 20:42:53 +0200

    feat(handlers): add rejection handlers

---
+++ src/main/scala/dixa/RejectionHandlers.scala
@@ -0,0 +1,16 @@
+package dixa.test
+
+import akka.http.scaladsl.model.StatusCodes.NotFound
+import akka.http.scaladsl.server.Directives.complete
+import akka.http.scaladsl.server.RejectionHandler
+
+object RejectionHandlers {
+  private val handler = RejectionHandler.newBuilder();
+
+  def rejectionHandler =
+    handler
+      .handleNotFound {
+        complete(NotFound, "path not found. Try making a GET request to /prime/:number")
+      }
+      .result()
+}
